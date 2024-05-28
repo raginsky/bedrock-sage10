4b51a5b1c855cc2400c863e4815ee8347e427e88
Michael Raginsky <michelr@inabit.com>
2024-05-28 09:34:07 +0300

    acf enabled defaults

--- web/app/themes/sage10/config/acf.php
+++ web/app/themes/sage10/config/acf.php
@@ -17,8 +17,15 @@ return [
     */
 
     'defaults' => [
-        // 'trueFalse' => ['ui' => 1],
-        // 'select' => ['ui' => 1],
+        // 'fieldGroup' => ['instruction_placement' => 'acfe_instructions_tooltip'],
+        // 'repeater' => ['layout' => 'block', 'acfe_repeater_stylised_button' => 1],
+        'trueFalse' => ['ui' => 1],
+        'select' => ['ui' => 1],
+        'postObject' => ['ui' => 1, 'return_format' => 'object'],
+        'accordion' => ['multi_expand' => 1],
+        // 'group' => ['layout' => 'table', 'acfe_group_modal' => 1],
+        'tab' => ['placement' => 'left'],
+        'sidebar_selector' => ['default_value' => 'sidebar-primary', 'allow_null' => 1]
     ],
 
     /*
