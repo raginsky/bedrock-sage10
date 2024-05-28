fe0da084758eb7dd794e0ff3a9b2fd7f49e01e5b
Michael Raginsky <michelr@inabit.com>
2024-05-28 10:34:08 +0300

    themeOptions starter template

--- web/app/themes/sage10/app/Options/ThemeOptions.php
+++ web/app/themes/sage10/app/Options/ThemeOptions.php
@@ -22,17 +22,92 @@ class ThemeOptions extends Field
     public $title = 'Theme Options | Options';
 
     /**
-     * The option page field group.
+     * Define the option page fields.
+     *
+     * @return array
      */
     public function fields(): array
     {
+        // Initialize the field group
         $themeOptions = Builder::make('theme_options');
 
+        // Header Tab
+        $themeOptions->addTab('Header', [
+            'placement' => 'top',
+        ]);
+
+        // Site Logo
         $themeOptions
-            ->addRepeater('items')
-                ->addText('item')
+            ->addImage('site_logo', [
+                'label' => 'Site Logo',
+                'instructions' => 'Upload the site logo.',
+                'return_format' => 'url',
+                'preview_size' => 'medium',
+                'wrapper' => [
+                    'width' => '50',
+                ],
+            ]);
+
+        // Footer Tab
+        $themeOptions->addTab('Footer', [
+            'placement' => 'top',
+        ]);
+
+        // Social Media Links
+        $themeOptions
+            ->addRepeater('social_media', [
+                'label' => 'Social Media Links',
+                'button_label' => 'Add Social Media Link',
+                'layout' => 'block',
+            ])
+                ->addText('platform', [
+                    'label' => 'Platform',
+                    'instructions' => 'Enter the social media platform (e.g., Facebook, Twitter).',
+                    'wrapper' => [
+                        'width' => '50',
+                    ],
+                ])
+                ->addUrl('url', [
+                    'label' => 'URL',
+                    'instructions' => 'Enter the URL to your social media profile.',
+                    'wrapper' => [
+                        'width' => '50',
+                    ],
+                ])
             ->endRepeater();
 
+
+        $themeOptions
+        ->addColorPicker('primary_color', [
+            'label' => 'Primary Color',
+            'instructions' => 'Select the primary color for the site.',
+            'wrapper' => [
+                'width' => '25',
+            ],
+        ]);
+
+    $themeOptions
+        ->addColorPicker('secondary_color', [
+            'label' => 'Secondary Color',
+            'instructions' => 'Select the secondary color for the site.',
+            'wrapper' => [
+                'width' => '25',
+            ],
+        ]);
+
+        // Custom Footer Text
+        $themeOptions
+            ->addWysiwyg('footer_text', [
+                'label' => 'Footer Text',
+                'instructions' => 'Enter custom text for the site footer.',
+                'media_upload' => 0,
+                'toolbar' => 'basic',
+                'wrapper' => [
+                    'width' => '100',
+                ],
+            ]);
+
+        // Return the built field group
         return $themeOptions->build();
     }
 }
