2e228ad6452d8b08bc5442ab4823c7cda019920d
Michael Raginsky <michelr@inabit.com>
2024-06-04 16:54:42 +0300

    hero and section refactor

--- web/app/themes/sage10/resources/views/acf/layouts/section.blade.php
+++ web/app/themes/sage10/resources/views/acf/layouts/section.blade.php
@@ -1,7 +1,15 @@
-@wrapper
-@container
-@layouts('components')
-@include ('acf.components.'. layout())
-@endlayouts
-@endcontainer
-@endwrapper
+@php
+  $component = 'acf-section';
+@endphp
+
+@wrapper('div')
+  <div class="relative z-1 {{ $component }}">
+    @container
+      <div class="relative z-2 {{ $component }}__container">
+        @layouts('components')
+          @include('acf.components.' . layout())
+        @endlayouts
+      </div>
+    @endcontainer
+  </div>
+@endwrapper('div')
