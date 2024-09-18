21c7e43042cc229d13a12ee46cd6298b3ad2444f
Michael Raginsky <michelr@inabit.com>
2024-09-18 13:45:45 +0300

    custom list refactor

--- web/app/themes/sage/app/Fields/Components/CustomList.php
+++ web/app/themes/sage/app/Fields/Components/CustomList.php
@@ -13,6 +13,7 @@ $custom_list
         'choices'    => [
             'default' => 'Default Style',
             'platform' => 'One Platform List',
+            'streamline' => 'Streamline your crypto',
         ],
         'wrapper'    => ['width' => 50],
     ])
@@ -33,31 +34,35 @@ $custom_list
     ])
     
     ->addTab("Main")
-    // Add Image field
     ->addImage('image', [
         'label'        => 'Image',
         'preview_size' => 'medium',
         'wrapper'      => ['width' => 20]
     ])
 
-    // Add Title field
     ->addTextarea('title', [
         'rows'      => 2,
         'new_lines' => 'br',
         'wrapper'   => ['width' => 40]
     ])
 
-    // Add Text field
     ->addWysiwyg('text', [
-        'label' => '',
-        'wrapper'   => ['width' => 40],
+        'label' => null,
+        'wrapper'   => ['width' => 40, 'class' => 'acf-custom-list-wysiwyg'],
         'media_upload' => 0,
         'toolbar' => 'basic',
-        'delay' => 1
+        'delay' => 1,
     ])
 
     ->addTab("Button")
     ->addFields(get_field_partial('fragments.button'))
+    ->addTab("Settings")
+    ->addText('class_name', [
+        'label' => 'Item class name',
+        'wrapper'     => ['width' => 15]
+    ])
+    ->addFields(get_field_partial('fragments.background'))
+    ->addFields(get_field_partial('fragments.colors'))
     ->endRepeater();
 
 return $custom_list;
--- web/app/themes/sage/resources/views/acf/components/custom-list.blade.php
+++ web/app/themes/sage/resources/views/acf/components/custom-list.blade.php
@@ -1,47 +1,55 @@
-@set($component, 'acf-list-' . get_sub_field('style'))
-@set($sliderClass, get_sub_field('slider') ? 'swiper' : '')
+@php
+$styleClass = get_sub_field('style');
+$component =  $styleClass ? ' acf-list-' . get_sub_field('style') : '';
+
+$sliderClass = get_sub_field('slider') ? 'swiper' : '';
+$slideClass = $sliderClass ? " $sliderClass-slider" : '';
+
+$className = $class_name ?? '';
+@endphp
 
 <ul class="acf-{{layout()}} {{$component}} {{$sliderClass}}">
 
     @hassub('slider')
-    <div class="acf-{{layout()}}-slider swiper-container" id="{{$component}}">
-    <div class="swiper-wrapper">
+        <div class="acf-{{layout()}}-slider swiper-container" id="{{$component}}">
+            <div class="swiper-wrapper">
     @endsub
 
     @fields('custom_list')
-        <li class="{{$component}}__item @if($sliderClass){{$sliderClass}}-slide @endif">
+        <li class="{{$component}}__item relative {{$slideClass}}{{$className}}{!!Page::textColor()!!}{!!Page::bg()!!}">
+            {!!Page::bgImage()!!}
             @hassub('image')
                 <div class="{{$component}}__image">
                     <img class="acf-image" src="@sub('image', 'url')" alt="@sub('image', 'alt')">
                 </div>
             @endsub
 
-                <div class="{{$component}}__content">
+            <div class="{{$component}}__content">
                 @hassub('title')
                     <div class="{{$component}}__title">
                         @sub('title')
                     </div>
                 @endsub
-    
+
                 @hassub('text')
                     <div class="{{$component}}__text">
                         @sub('text')
                     </div>
                 @endsub
-    
-                @hassub('link')
-                <div class="{{$component}}__button">
-                    @include('acf.fragments.button')
-                </div>
-                @endsub
-                </div>
-        </li>
 
+                @hassub('link')
+                    <div class="{{$component}}__button">
+                        @include('acf.fragments.button')
+                    </div>
+                @endsub
+            </div>
+        </li>
     @endfields
 
     @hassub('slider')
+            </div>
         </div>
-    </div>
+
         <div class="acf-{{layout()}}__navigation">
             <div class="swiper-button-next {{$component}}-next">
                 @include('svg.acf.arrow-right')
