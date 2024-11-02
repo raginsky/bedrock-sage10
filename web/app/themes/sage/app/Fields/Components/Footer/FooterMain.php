1c364ae1b76e1fc6149be517b98e9badb6e420ee
Michael Raginsky <michelr@inabit.com>
2024-11-02 11:57:25 +0200

    remove correct fields from hero and footer

--- web/app/themes/sage/app/Fields/Components/Footer/FooterMain.php
+++ web/app/themes/sage/app/Fields/Components/Footer/FooterMain.php
@@ -9,6 +9,7 @@ $footer_main = new FieldsBuilder( 'footer_main' );
 $footer_main
 	->addFields( get_field_partial( 'components.footer.footerBuilder' ) )
 	->addFields( get_field_partial( 'partials.settings' ) )
-	->removeField('padding');
+	->removeField('padding_top')
+	->removeField('padding_bottom');
 
 return $footer_main;
--- web/app/themes/sage/app/Fields/Layouts/Hero.php
+++ web/app/themes/sage/app/Fields/Layouts/Hero.php
@@ -9,6 +9,7 @@ $hero = new FieldsBuilder('hero');
 $hero
    ->addFields(get_field_partial('partials.builder'))
    ->addFields(get_field_partial('partials.settings'))
-   ->removeField('padding');
+   ->removeField('padding_top')
+   ->removeField('padding_bottom');
 
 return $hero;
