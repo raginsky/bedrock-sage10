46cc8f471e589f519278008db311e0e2059a6879
Michael Raginsky <michelr@inabit.com>
2024-11-15 15:56:28 +0200

    hide unwanted links

--- web/app/themes/sage/app/setup.php
+++ web/app/themes/sage/app/setup.php
@@ -123,3 +123,27 @@ add_action('widgets_init', function () {
         'id' => 'sidebar-footer',
     ] + $config);
 });
+
+/**
+ * Redirect from single attachment or author page to parent or homepage.
+ *
+ * @see https://www.wpexplorer.com/disable-image-page/
+ */
+add_action('template_redirect', function () {
+    global $wp_query;
+
+    if ( is_author() ) {
+        wp_redirect(get_option('home'), 301); 
+        exit; 
+    }
+    if (is_attachment()) {
+        global $post;
+        if ($post && $post->post_parent) {
+            wp_redirect(esc_url(get_permalink($post->post_parent)), 301);
+            exit();
+        } else {
+            wp_redirect(esc_url(home_url('/')), 301);
+            exit();
+        }
+    }
+});
