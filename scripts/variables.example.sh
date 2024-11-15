70ae488354b92d2c76e027653cec9b3d6f6c9d43
Michael Raginsky <michelr@inabit.com>
2024-11-15 15:54:05 +0200

    scripts logic update, remote and local separation, uploads fetch

--- scripts/variables.example.sh
+++ scripts/variables.example.sh
@@ -9,3 +9,9 @@ PROJECT_ROOT=~/wordpress/wpbase/app/bedrock
 
 # Retrieve the MySQL socket path dynamically
 SOCKET_PATH=$(find ~/.config/Local/run -type s -name "mysqld.sock" 2>/dev/null | head -n 1)
+
+PROD_USER=
+PROD_SERVER_IP=
+PROD_SERVER="${PROD_USER}@${PROD_SERVER_IP}"
+PROD_DIRECTORY=""
+PROD_DOCKER_USER="www-data"
